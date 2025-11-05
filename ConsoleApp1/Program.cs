9ef5917c4a3f73f9516a5abb113e9d58d0bcf522
jehua <docjehua22@gmail.com>
2025-11-05 12:09:36 +0700

    Logic Excercise

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.Write("Masukkan banyaknya n :");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i<= number; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        System.Console.Write("foobar, ");
+    }else if (i % 3 == 0)
+    {
+        System.Console.Write("foo, ");
+    }else if (i % 5 == 0)
+    {
+        System.Console.Write("bar, ");
+    }else
+    {
+        System.Console.Write($"{i}, ");
+    }
+}
+
