563558517127f2a807bc0530c1feabe728775bb5
jehua <docjehua22@gmail.com>
2025-11-06 13:02:43 +0700

    menambahkan Project Dip

---
+++ Solid/Dependency Inversion Principle (DIP)/Dip Project Notification Message/Program.cs
@@ -0,0 +1,31 @@
+﻿INotificationSender notif = new EmailSender();
+NotificationService service = new NotificationService(notif);
+service.NotificationSend();
+
+
+public interface INotificationSender
+{
+    void ProcessNotification();
+}
+public class EmailSender :INotificationSender
+{
+    public void ProcessNotification()
+    {
+        System.Console.WriteLine("Email sent: Halo, pesanan kamu sudah dikirim!");
+    }
+}
+
+public class NotificationService
+{
+    private readonly INotificationSender _notification;
+    public NotificationService(INotificationSender notification)
+    {
+        _notification = notification;
+    }
+    public void NotificationSend()
+    {
+        _notification.ProcessNotification();
+        
+    }
+
+}
