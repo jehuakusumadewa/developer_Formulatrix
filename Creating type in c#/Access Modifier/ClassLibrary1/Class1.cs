0352cac79fc5bdc5b43dd90c4f305c70ec551ae7
jehuakusumadewa <docjehua22@gmail.com>
2025-11-08 05:56:14 +0700

    mencoba menambahkan friend assembly

---
+++ Creating type in c#/Access Modifier/ClassLibrary1/Class1.cs
@@ -0,0 +1,11 @@
+﻿using NgulikAccessModifier;
+
+
+public class TestFromLibrary
+{
+    public void Run()
+    {
+        c1 obj = new c1();           // Bisa diakses
+        SubClass sub = new SubClass();
+    }
+}
--- Creating type in c#/Access Modifier/ngulik access modifier/Program.cs
+++ Creating type in c#/Access Modifier/ngulik access modifier/Program.cs
@@ -1,19 +1,30 @@
-﻿class c1 { } // class yang tidak didefinisi access modifiernya maka akan otomatis internal
+﻿namespace NgulikAccessModifier;
+
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Running tests...");
+    }
+}
+
+class c1 { } // class yang tidak didefinisi access modifiernya maka akan otomatis internal
 // acces modifier internal sendiri berarti class dapat di akses jika dalam 1 assembly atau 
 // bahasa mudahnya masih ada dalam 1 project, maka class bisa di akses.
 
 
 
-class A
-{
-    int bisa; // disini field/ property auto private kalo gk di set. Yang dapat mengaksesnya hanya dalam 1 class
+// class A
+// {
+//     int bisa; // disini field/ property auto private kalo gk di set. Yang dapat mengaksesnya hanya dalam 1 class
 
-}
+// }
 
-class B
-{
-    internal int berat; // disini yang bisa mengaksesnya dalam 1 assembly atau dalam 1 project
-}
+// class B
+// {
+//     internal int berat; // disini yang bisa mengaksesnya dalam 1 assembly atau dalam 1 project
+// }
 
 class BaseClass
 {
