d8f1f1a3ac5bb0ac95f5eb86d17d625fc304112f
jehuakusumadewa <docjehua22@gmail.com>
2025-11-09 12:48:01 +0700

    nyoba enums

---
+++ Creating type in c#/Enums/Enums_Excercise/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Hari day = Hari.Senin; 
+        Console.WriteLine(day);
+    }
+}
+
+
+
+public enum Hari
+{
+    Senin,
+    Selasa,
+    Rabu,
+    Kamis,
+    Jumat,
+    Sabtu,
+    Minggu
+}
