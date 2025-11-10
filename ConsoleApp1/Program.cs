72bf1412cebff0f687aec567734474fac43a72de
jehua <docjehua22@gmail.com>
2025-11-10 08:12:04 +0700

    logic excercise versi 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,16 +4,33 @@ int number = int.Parse(Console.ReadLine());
 
 for (int i = 1; i<= number; i++)
 {
-    if (i % 3 == 0 && i % 5 == 0)
+    if (i % 7 == 0 && i % 3 == 0 && i % 5 == 0)
+    {
+        System.Console.Write("foobarjazz, ");
+    }else if (i % 7 == 0 && i % 3 == 0)
+    {
+        System.Console.WriteLine("foojazz, ");
+    }else if (i % 7 == 0 && i % 5 == 0)
+    {
+        System.Console.WriteLine("barjazz, ");
+    }
+    else if (i % 3 == 0 && i % 5 == 0)
     {
         System.Console.Write("foobar, ");
-    }else if (i % 3 == 0)
+    }
+    else if (i % 3 == 0)
     {
         System.Console.Write("foo, ");
-    }else if (i % 5 == 0)
+    }
+    else if (i % 5 == 0)
     {
         System.Console.Write("bar, ");
-    }else
+    }
+    else if (i % 7 == 0)
+    {
+        System.Console.WriteLine("jazz, ");
+    }
+    else
     {
         System.Console.Write($"{i}, ");
     }
