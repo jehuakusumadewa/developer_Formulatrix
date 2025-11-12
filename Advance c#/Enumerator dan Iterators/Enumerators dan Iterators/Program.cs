57a44b9d34fda2681f2d6b8182ec2980fc674d78
jehua <docjehua22@gmail.com>
2025-11-12 15:02:21 +0700

    menambahkan enumerators dan iterators

---
+++ Advance c#/Enumerator dan Iterators/Enumerators dan Iterators/Program.cs
@@ -0,0 +1,57 @@
+﻿public interface IEnumerable
+{
+    IEnumerator GetEnumerator();
+}
+
+public interface IEnumerator
+{
+    bool MoveNext();
+    object Current { get; }
+    void Reset();
+}
+public class NumberCollection : IEnumerable
+{
+    private int[] numbers;
+
+    public NumberCollection(int[] nums)
+    {
+        numbers = nums;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return new NumberEnumerator(numbers);
+
+}
+    public class NumberEnumerator : IEnumerator
+    {
+        private int[] numbers;
+        private int position = -1;
+        public NumberEnumerator(int[] nums)
+        {
+            numbers = nums;
+        }
+        public bool MoveNext()
+        {
+            position++;
+            return position < numbers.Length;
+        }
+        public object Current
+        {
+            get
+            {
+                if (position < 0 || position >= numbers.Length)
+                    throw new InvalidOperationException();
+                return numbers[position];
+            }
+        }
+         public void Reset()
+        {
+                position = -1;
+        }
+        
+        
+
+    }
+    
+}
