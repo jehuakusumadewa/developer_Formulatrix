c1e0400a7a29d9d398ccc534471a30e77506cbb3
jehuakusumadewa <docjehua22@gmail.com>
2025-11-18 05:58:15 +0700

    menambahkan huzz

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-System.Console.Write("Masukkan banyaknya n :");
+﻿System.Console.Write("Masukkan banyaknya n :");
 int number = int.Parse(Console.ReadLine());
 
 for (int i = 1; i<= number; i++)
@@ -7,12 +6,42 @@ for (int i = 1; i<= number; i++)
     if (i % 7 == 0 && i % 3 == 0 && i % 5 == 0)
     {
         System.Console.Write("foobarjazz, ");
-    }else if (i % 7 == 0 && i % 3 == 0)
+    }
+    else if (i % 9 == 0 && i % 7 == 0 && i % 3 == 0 && i % 5 == 0)
     {
-        System.Console.WriteLine("foojazz, ");
-    }else if (i % 7 == 0 && i % 5 == 0)
+        System.Console.Write("foobarjazzhuzz, ");
+    }
+    else if (i % 9 == 0 && i % 7 == 0 && i % 3 == 0)
     {
-        System.Console.WriteLine("barjazz, ");
+        System.Console.Write("foojazzhuzz, ");
+    }
+    else if (i % 9 == 0 && i % 7 == 0 && i % 5 == 0)
+    {
+        System.Console.Write("barjazzhuzz, ");
+    }
+    else if (i % 9 == 0 && i % 3 == 0 && i % 5 == 0)
+    {
+        System.Console.Write("foobarhuzz, ");
+    }
+    else if (i % 7 == 0 && i % 3 == 0)
+    {
+        System.Console.Write("foojazz, ");
+    }
+    else if (i % 7 == 0 && i % 5 == 0)
+    {
+        System.Console.Write("barjazz, ");
+    }
+    else if (i % 9 == 0 && i % 3 == 0)
+    {
+        System.Console.Write("foohuzz, ");
+    }
+    else if (i % 9 == 0 && i % 5 == 0)
+    {
+        System.Console.Write("barhuzz, ");
+    }
+    else if (i % 9 == 0 && i % 7 == 0)
+    {
+        System.Console.Write("jazzhuzz, ");
     }
     else if (i % 3 == 0 && i % 5 == 0)
     {
@@ -28,11 +57,14 @@ for (int i = 1; i<= number; i++)
     }
     else if (i % 7 == 0)
     {
-        System.Console.WriteLine("jazz, ");
+        System.Console.Write("jazz, ");
+    }
+    else if (i % 9 == 0)
+    {
+        System.Console.Write("huzz, ");
     }
     else
     {
         System.Console.Write($"{i}, ");
     }
 }
-
