d96f8fc5507f63b8725296eaad6e2ab79e3318fa
jehua <docjehua22@gmail.com>
2025-12-04 15:07:15 +0700

    menambahkan asp . net core mvc

---
+++ MVC/aspnetcore/Models/TodoViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetCoreTodo.Models
+{
+    public class TodoViewModel
+    {
+        public TodoItem[] items {get; set;}
+        
+    }
+}
---
+++ MVC/aspnetcore/Services/TodoItemService.cs
@@ -0,0 +1,60 @@
+using	System;
+using	System.Collections.Generic;
+using	System.Linq;
+using	System.Threading.Tasks;
+using	AspNetCoreTodo.Data;
+using	AspNetCoreTodo.Models;
+using	Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTodo.Services
+{
+    public class TodoItemService: ITodoItemService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TodoItem[]> GetIncompleteItemsAsync(ApplicationUser user)
+        {
+            return await _context.items
+            .Where(x => x.IsDone == false)
+            .ToArrayAsync();
+        }
+
+        public async Task<bool> AddItemAsync(TodoItem newItem)
+        {
+            newItem.Id = Guid.NewGuid();
+            newItem.IsDone = false;
+            newItem.DueAt = DateTimeOffset.Now.AddDays(3);
+
+            _context.items.Add(newItem);
+            
+            var saveResult = await _context.SaveChangesAsync();
+            
+            return saveResult == 1;
+        }
+
+        public async Task<bool> MarkDoneAsync(Guid id)
+        {
+            var item = await _context.items
+                .Where(x => x.Id == id)
+                .SingleOrDefaultAsync();
+
+            if (item == null) 
+                return false;
+
+            item.IsDone = true;
+
+            var saveResult = await _context.SaveChangesAsync();
+            
+            return saveResult == 1; // One entity should have been updated
+        }
+
+
+    }
+
+
+}
